a728cf6c0dd8a04a401a1311037a4824c7e2cdc2
Agus Syahril Mubarok <mrschwartx@gmail.com>
2025-09-25 06:19:24 -0500

    feat(main): revamp code

--- devops/container/UbuntuDocker/setup-docker.sh
+++
@@ -1,8 +0,0 @@
-# Build Docker Images
-docker build -t ubuntu-jammy .
-
-# Run Docker Container
-docker run -d -p 2222:22 --name my-ubuntu-jammy ubuntu-jammy
-
-# SSH into Container
-ssh myuser@localhost -p 2222
